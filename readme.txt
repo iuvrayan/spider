b9878265355fe5c7e1259ecd3d32673a5d22ccaa
unknown <irayan@gmail.com>
2019-08-07 21:48:52 +0100

    test ws

---
+++ readme.txt
@@ -0,0 +1,5 @@
+rustup override set nightly
+
+git clone https://github.com/SergioBenitez/Rocket
+cd Rocket
+git checkout v0.4.2
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+//use std::path::PathBuf;
+//use rocket::http::RawStr;
+
+#[get("/")]
+fn crawl() -> String {
+    //let decoded = domain.url_decode(); 
+    //format!("Domain : , {}", domain.as_str())
+    format!("Domain : , {}", "Hello World !")    
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![crawl]).launch();  
 }
